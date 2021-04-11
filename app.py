0a995f30f415a97b8de28b1c64f629956fb4329d
Cameron Hirsh <camjhirsh@gmail.com>
2021-04-11 16:17:59 -0700

    Testing text and data entry

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-immport streamlit as st
+# Streamlit App #
+
+# Import Necessary Libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Title and Headings
+st.title("Product Title")
+
+st.header("""Does your company take the mental health of its employees as seriously as physical health?""")
+
+st.subheader("Mental Health in the Tech Industry")
+
+st.text(""" These are some stats on the state of mental health in tech rn""")
+
+st.subheader("What [our product] can do your your employees")
+
+st.text(""" This is what we can do for your company and people""")
+
+# Load data
+df = pd.read_csv("survey.csv")
+
+# Display survey
+st.write(df)
+
+
+st.line_chart(df)
